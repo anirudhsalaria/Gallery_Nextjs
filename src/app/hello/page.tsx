2959bc13fcbed2e5858303e0fca8aabec9cd98de
Anirudh Salaria <salariaanirudh@gmail.com>
2023-07-05 15:16:23 +0530

    adding error page

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,5 +1,6 @@
 export default async function Page() {
     await new Promise((resolve) => setTimeout(resolve,1000));
+    throw Error("Hiyaaa");
     return(
         <div>
             Hello World
